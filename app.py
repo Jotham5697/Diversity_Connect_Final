708d0fbba09fe987d9c8ae0ef05ddb4b40c88dfe
Jotham <j.kriakos@lemanmanhattan.com>
2021-07-29 15:25:41 +0000

    initial push

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-# ---- YOUR APP STARTS HERE ----
-# -- Import section --
 from flask import Flask
 from flask import render_template
-from flask import request
-import model
 
-
-# -- Initialization section --
 app = Flask(__name__)
 
-
-# -- Routes section --
 @app.route('/')
 @app.route('/index')
-def index():
-    return render_template('index.html')
+def test3():
+    return render_template("index.html")
+
+@app.route("/podcasts")
+def podcasts():
+    return render_template("/podcasts.html")
+
+@app.route("/chat")
+def chat():
+    return render_template("/chat.html")
